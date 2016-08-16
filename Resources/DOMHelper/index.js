5849a6e332f49767f6a169e643461f09ef5ad339
sirudduk <sirudduk@jungkyoonui-MacBook-Pro.local>
2016-08-16 10:21:44 +0900

    swich문 업로드

--- Resources/DOMHelper/index.js
+++ Resources/DOMHelper/index.js
@@ -1,11 +1,35 @@
+/*! DOMHelper.js © sirudduk 2016 */
 
+// 자바스크립트의 모든 데이터 유형을 올바르게 감지할 수 있는 헬퍼 함수
+function isType(data) {
+   return Object.prototype.toString.call(data).slice(8,-1).toLowerCase();
+}
 
+// 데이터 간 동등한지 유무 파악 헬퍼 함수
+function equal(data1, data2) {
+   return data1 == data2;
+}
+
+// 데이터 간 완전하게 동등한지 유무 파악 헬퍼 함수
+function strictEqual(data1, data2) {
+   return data1 === data2;
+}
+
+function throwError(type1, type2, err_msg) {
+  err_msg = err_msg || '기본 오류 메시지';
+  if ( isType(type1) !== type2 ) { throw new Error(err_msg); }
+}
+
+function validDate(data, type) {
+  throwError(type, 'string'); // 오류 발생 시 멈추고 화면에 오류 메시지 출력
+  return strictEqual( isType(data), type );
+}
 
 /**
  * prependChild(부모노드, 자식노드)
  * 부모노드의 첫번째 자식노드로 삽입한다.
  * ---------------------------------------------
- * @작성자    sirudduk
+ * @작성자    yamoo9
  * @버전     0.0.1
  * @param  {ELEMENT_NODE}  parent_node 부모노드
  * @param  {ELEMENT_NODE}  child_node  자식노드
@@ -19,7 +43,7 @@ function prependChild(parent_node, child_node) {
  * insertAfter(목표노드, 삽입노드)
  * 목표노드 뒤에 삽입노드를 추가한다.
  * ---------------------------------------------
- * @작성자    sirudduk
+ * @작성자    yamoo9
  * @버전     0.0.1
  * @param  {ELEMENT_NODE}  target_node  목표노드
  * @param  {ELEMENT_NODE}  insert_node  삽입노드
@@ -45,7 +69,6 @@ function queryAll(selector_str, context) {
   if (!context) { context = document; }
   return context.querySelectorAll(selector_str);
 }
-
 
 function query(selector, parent) {
   return queryAll(selector, parent)[0];
@@ -76,4 +99,48 @@ function createNode(el_name, text) {
     el_node.appendChild(text_node);
   }
   return el_node;
+}
+
+
+// ------------------------------------------------
+// 웹 브라우저에서 계산된 CSS 스타일 값 가져오는 방법
+// ------------------------------------------------
+// 비 표준 MS IE 방식 (IE 8-)
+// 대상.currentStyle.스타일속성
+// ------------------------------------------------
+// 표준 W3C 방식 (IE 9+)
+// window.getComputedStyle(대상,가상요소).스타일속성
+// ------------------------------------------------
+function getStyle(el, property, pseudo) {
+  var value, el_style;
+  // 유효성 검사
+  if ( el.nodeType !== 1 ) {
+    console.error('첫번째 인자 el은 요소노드여야 합니다.');
+  }
+  if ( typeof property !== 'string' ) {
+    console.error('두번째 인자 property는 문자열이야 합니다.');
+  }
+  if ( pseudo && typeof pseudo !== 'string' ) {
+    console.error('세번째 인자 pseudo는 문자열이야 합니다.');
+  }
+
+  // CSS 속성 이름이 카멜케이스화
+  property = camelCase(property);
+
+  if ( window.getComputedStyle ) {
+    el_style = window.getComputedStyle(el,pseudo);
+    if (pseudo && el_style.content === '') {
+      return null;
+    }
+    value = el_style[property];
+  } else {
+    value = el.currentStyle[property];
+  }
+  return value;
+}
+
+function camelCase(text) {
+   return text.replace(/(\s|-|_)./g, function($1){
+      return $1.replace(/(\s|-|_)/g,'').toUpperCase();
+   });
 }
